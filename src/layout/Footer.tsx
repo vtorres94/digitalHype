8284677bfd08552bd92f81f4b9dca4da765f43ff
mtorres94 <vladimir.tordavi@gmail.com>
2023-03-25 00:51:06 -0600

    add footer styles

--- src/layout/Footer.tsx
+++ src/layout/Footer.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import '../css/footer.css'
 
 const Footer: React.FC = () => {
     return(
-        <>
-        
-        </>
+        <div className="footer">
+
+        </div>
     );
 }
 
